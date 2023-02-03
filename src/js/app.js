60d9983d12aa4dc7332cf81034f714e4cca5d788
mr-dim-ka <kalinin9.7@mail.ru>
2023-02-03 18:26:05 +0600

    added section services, adaptive services and fix in header, mainpage

--- src/js/app.js
+++ src/js/app.js
@@ -46,7 +46,7 @@ flsFunctions.fullVHfix();
 Документация: https://template.fls.guru/template-docs/modul-taby.html
 Сниппет (HTML): tabs
 */
-// flsFunctions.tabs();
+flsFunctions.tabs();
 
 /*
 Модуль "показать еще"
